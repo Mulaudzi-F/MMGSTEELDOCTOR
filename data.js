548c190427060c691e22e9669a29140c94e2dca1
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-09-23 17:10:32 +0200

    made the dialog overlay for boilers

---
+++ data.js
@@ -0,0 +1,309 @@
+
+export const boilers =[
+
+    {
+        image: "boiler--1",
+        id: "01",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "boiler--2",
+        id: "02",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "boiler3",
+        id: "03",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--4",
+        id: "04",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--5",
+        id: "05",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--6",
+        id: "06",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--7",
+        id: "07",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--8",
+        id: "08",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--9",
+        id: "09",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--10",
+        id: "10",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--11",
+        id: "11",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--12",
+        id: "12",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--13",
+        id: "13",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--14",
+        id: "14",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--15",
+        id: "15",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--16",
+        id: "16",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--17",
+        id: "17",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--18",
+        id: "18",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--19",
+        id: "19",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--20",
+        id: "20",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+    {
+        image: "Boiler--21",
+        id: "21",
+        description: "MMG Steel doctor at boiler installation service"
+    },
+] 
+
+const pipes =[
+
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    }
+    
+] 
+
+const gysers =[
+
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    },
+    {
+        image: "",
+        id: "",
+        description: "z"
+    }
+    
+]
